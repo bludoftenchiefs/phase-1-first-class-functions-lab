7fc10b322509711464a84d32573ab073e8a65ca8
rilith <bludoftenchiefs@protonmail.com>
2021-05-08 10:48:30 -0500

    commit tree 9dc11a7f315461cceb699b51c9375285d3f312f6

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+ // Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function() {
+        return drivers.slice(0,2)
+    }
+const returnLastTwoDrivers = function() {
+        return drivers.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier() {
+        let fareQuintupler = function(fare, x) {
+                return fare * 5;
+        }
+        return fareQuintupler;
+}
+
+function fareDoubler(createFareMultiplier) {
+        return createFareMultiplier * 2;
+}
+
+function fareTripler(createFareMultiplier) {
+        return createFareMultiplier * 3;
+}
+
+// function selectDifferentDrivers(arrayOfDrivers, driversNames) {
+//         driverNames(arrayOfObjects);
+function selectDifferentDrivers(arrayOfDrivers, driverFunction){
+    return driverFunction(arrayOfDrivers);
+}
+
